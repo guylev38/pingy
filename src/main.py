9dae61de50176c098708cc8a60b4226f1999a458
guydo <@>
2025-09-22 13:51:59 +0300

    progress

--- src/main.py
+++ src/main.py
@@ -7,17 +7,26 @@ Date: 20/09/2025
 
 # ----- Imports ----- # 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # ----- Globals ----- #
 
 app = FastAPI()
+templates = Jinja2Templates(directory=""
+                                      "assets/templates")
+
+# ----- Config ----- #
+
+app.mount("/assets/static", StaticFiles(directory="assets/static"), name="static")
 
 # ----- Functions ----- # 
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    context = {"request": request, "message": "Hello!"}
+    return templates.TemplateResponse("index.html", context)
 
